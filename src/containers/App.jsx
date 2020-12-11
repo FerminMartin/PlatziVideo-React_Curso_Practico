11365203543ffc36e60701f5d61fa161bbb8cb41
Fermin Martin <fermartorr01@gmail.com>
2020-12-11 10:42:57 +0100

    PLATZIVIDEO: Custom Hooks. Saco la lógica de mi componente App.jsx y lo aislo en un componente con una funcion(initialstate.js) en una nueva carpeta llamada hooks. Aquí pego la lógica y abstraigo la API. Por último le paso la función (custom Hook) al componente creando una constante initialState = useInitialState(API). Por último sustituyo de los elementos del componente videos x initialState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,35 +5,30 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => {
-  const [ videos, setVideos ] = useState([]);
+const API = 'http://localhost:3000/initialState';
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initialState')
-      .then(response => response.json())
-      .then(data => setVideos(data));
-  }, []);
+const App = () => {
+  const initialState = useInitialState(API);
   return(
     <div className="App">
       <Header />
       <Search />
-
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-
+      {initialState.mylist.length > 0 &&
+        <Categories title="Mi Lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      }
+      
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {initialState.trends.map(item => 
+            <CarouselItem key={item.id} {...item}/>
+          )}
         </Carousel>
       </Categories>
 
